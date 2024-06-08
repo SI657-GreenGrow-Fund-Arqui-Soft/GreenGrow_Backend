4e6877076b85f67e084a984a4f28b9f39977a90e
caszarjos <zaratecastro2003@hotmail.com>
2024-06-08 12:47:14 -0500

    (fix): Refactor api gateway routing

---
+++ api-gateway-eureka/src/main/java/com/greengrow/backend/apigateway/routes/Routes.java
@@ -0,0 +1,67 @@
+package com.greengrow.backend.apigateway.routes;
+
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+@Configuration
+public class Routes {
+
+    @Bean
+    public RouterFunction<ServerResponse> articlesServiceRoute() {
+        return GatewayRouterFunctions.route("articles-service")
+                .route(RequestPredicates.path("/api/green-grow/v1/articles")
+                , HandlerFunctions.http("http://localhost:8080"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> coursesServiceRoute() {
+        return generalGreenGrowRoute(
+                "courses-service",
+                "course",
+                "http://localhost:8081"
+        );
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> postsServiceRoute() {
+        return generalGreenGrowRoute(
+                "posts-service",
+                "posts",
+                "http://localhost:8082"
+        );
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> profilesServiceRoute() {
+        return generalGreenGrowRoute(
+                "profiles-service",
+                "profile",
+                "http://localhost:8083"
+        );
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> trendsServiceRoute() {
+        return generalGreenGrowRoute(
+                "trends-service",
+                "trends",
+                "http://localhost:8084"
+        );
+    }
+
+    static private RouterFunction<ServerResponse> generalGreenGrowRoute(
+            String routeId, String path, String url
+    ) {
+        String basePath = "/api/green-grow/v1/";
+        return GatewayRouterFunctions.route(routeId)
+                .route(RequestPredicates.path(basePath + path)
+                        , HandlerFunctions.http(url))
+                .build();
+    }
+}
